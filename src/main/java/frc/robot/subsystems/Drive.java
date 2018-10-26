2a9ca8414e5a8657acbb47d6f9dc318c15ec55dc
19aanand <vasdanand@gmail.com>
2018-10-26 19:24:39 -0400

    Added functionality to Drive class

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -8,7 +8,6 @@ public class Drive extends Subsystem {
         1. 6 motors to power drivetrain
         2. 2 encoders for each side of the drivetrain
         3. 6 speed controllers to regulate motor rpm
-        4. 
     */
     public Drive() {
 
@@ -18,7 +17,21 @@ public class Drive extends Subsystem {
 
     @Override
     protected void initDefaultCommand() {
+        //Instantiate 6 speed controller objects
+        /*
+        TalonSRX mLeftMotor1 = new TalonSRX();
+        TalonSRX mLeftMotor2 = new TalonSRX();
+        TalonSRX mLeftMotor3 = new TalonSRX();
+        TalonSRX mRightMotor1 = new TalonSRX();
+        TalonSRX mRightMotor2 = new TalonSRX();
+        TalonSRX mRightMotor3 = new TalonSRX();
+        */
+    }
 
+    public void setMotion(double horizontal, double vertical)
+    {
+        //Calculate resultant velocity based on values of horizontal and vertical
+        //Sets speed controller values based on the resultant velocity
     }
 
 }
