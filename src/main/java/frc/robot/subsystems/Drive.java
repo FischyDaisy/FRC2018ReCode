6425d6678ce993a990199d851f3cfa66168c6ce5
19aanand <vasdanand@gmail.com>
2018-10-26 19:10:39 -0400

    Initial Drive class changes

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -4,6 +4,12 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Drive extends Subsystem {
 
+    /*
+        1. 6 motors to power drivetrain
+        2. 2 encoders for each side of the drivetrain
+        3. 6 speed controllers to regulate motor rpm
+        4. 
+    */
     public Drive() {
 
 
