c42262ce310ecf04e94ac2751abdbe81e6ec970f
TechOh <43895894+TechOh@users.noreply.github.com>
2018-10-26 19:03:58 -0400

    changes
    things

--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -5,14 +5,18 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Wrist extends Subsystem {
 
     public Wrist() {
-
-
-        
+        /*
+            create method that tells to turn the wrist
+            limit switch on wrist to prevent it from turning too much
+        */
     }
 
     @Override
     protected void initDefaultCommand() {
-
+        /*
+            create variable mMotor
+            position function determines position based on mMotor value
+        */
     }
 
 }
