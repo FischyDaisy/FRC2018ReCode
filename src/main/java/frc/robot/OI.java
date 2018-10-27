061ffafcaadaa5992cbfa744f461f96f2af1bf7e
19aanand <vasdanand@gmail.com>
2018-10-26 20:12:36 -0400

    Instantiating buttons for operator and driver

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -2,11 +2,28 @@ package frc.robot;
 
 public class OI {
 
-    public OI() {
+    public OI() 
+    {
+        /*
+        Instantiate left joystick of driver controller. This will dictate forwards/backwards movement
+        Instantiate right joystick of driver controller. This will dictate turning on the robot.
+        Instantiate X-button on driver conroller. This will actuate the popper piston.
+        Instantiate right bumper (R1) on driver controller. This will shoot the climber up.
+        Intantiate left bupmer (L1) on driver controller. This will retract the climber.
+        */
 
+        /*
+        Instantaite left joystick for operator controller. This will control forearm movement.
+        Instantiate right joystick for operator controller. This will control wrist movement.
+        Instantiate X-Button for operator controller. This will move the arm into the stowed position.
+        Instantiate A-Button for operator controller. This will move the arm into the switch scoring position.
+        Instantiate Y-Button for operator controller. This will move the arm into the scale scoring position in the forwards direction.
+        Instantiate B-Button for operator controller. This will move the arm into the scale scoring position in the backwards direction.
+        */
     }
 
     public void run() {
 
     }
+
 }
